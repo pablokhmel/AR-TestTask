2572fa73d13b13355a4b5f4d71bdc8fb94df1925
pablokhmel <pablokhmelevskiy@gmail.com>
2022-10-21 22:01:12 +0200

    Initial Commit

---
+++ AR-TestTask/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR-TestTask
+//
+//  Created by MacBook on 21.10.2022.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
