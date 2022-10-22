1fb0d84fe1c42f7a594c86b5c6cd98c594e27cb1
pablokhmel <pablokhmelevskiy@gmail.com>
2022-10-22 17:39:05 +0200

    initial commit

--- AR-TestTask/ViewController.swift
+++ AR-TestTask/ViewController.swift
@@ -10,15 +10,32 @@ import RealityKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    let arView: ARView = {
+        let view = ARView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupSubviews()
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+    }
+
+    func setupSubviews() {
+        view.addSubview(arView)
+
+        NSLayoutConstraint.activate([
+            arView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            arView.topAnchor.constraint(equalTo: view.topAnchor),
+            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
