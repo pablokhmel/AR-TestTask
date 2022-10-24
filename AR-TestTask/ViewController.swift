9ff5484b1f750260cbcee9a6dcd481acd854f4de
pablokhmel <pablokhmelevskiy@gmail.com>
2022-10-24 13:12:59 +0200

    Adding elements done

--- AR-TestTask/ViewController.swift
+++ AR-TestTask/ViewController.swift
@@ -7,35 +7,88 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
-    let arView: ARView = {
+    private lazy var arView: ARView = {
         let view = ARView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tap)
         return view
+    }()
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "text"
+        label.textColor = .black
+        label.backgroundColor = .white
+        return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupSubviews()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        startPlaneDetection()
+        print("loaded")
     }
 
-    func setupSubviews() {
+    override func viewDidLayoutSubviews() {
+        arView.frame = view.bounds
+    }
+
+    private func setupSubviews() {
         view.addSubview(arView)
+        arView.addSubview(label)
 
         NSLayoutConstraint.activate([
             arView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             arView.topAnchor.constraint(equalTo: view.topAnchor),
-            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            label.centerXAnchor.constraint(equalTo: arView.centerXAnchor),
+            label.topAnchor.constraint(equalTo: arView.topAnchor, constant: 40)
         ])
+    }
+
+    private func startPlaneDetection() {
+        arView.automaticallyConfigureSession = true
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        config.environmentTexturing = .automatic
+        arView.session.run(config)
+    }
+
+    private func addModel(to point: simd_float3) {
+        // Creating object
+        let cube = MeshResource.generateBox(size: 0.05)
+        let material = SimpleMaterial(color: .yellow, isMetallic: false)
+        let entity = ModelEntity(mesh: cube, materials: [material])
+
+        // Adding object
+        let anchor = AnchorEntity(world: point)
+        anchor.addChild(entity)
+
+        // Adding anchor to view
+        arView.scene.addAnchor(anchor)
+    }
+
+    @objc
+    private func handleTap(_ sender: UITapGestureRecognizer?) {
+        guard let tapLocation = sender?.location(in: arView) else { return }
+
+        guard let query = arView.makeRaycastQuery(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal) else { print("Query"); return }
+
+        guard let ray = arView.session.raycast(query).first else { print("Ray"); return }
+
+        let position = simd_make_float3(ray.worldTransform.columns.3)
+
+        addModel(to: position)
     }
 }
---
+++ AR-TestTask/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  AR-TestTask
+//
+//  Created by MacBook on 23.10.2022.
+//
+
+import Foundation
+
+class ViewModel {
+    func 
+}
